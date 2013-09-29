5225eb44612d1e87f2595e49dbb3d24721720fc2
Jason Friedman <write.to.jason@gmail.com>
2013-09-29 13:25:16 +0300

    Fix port numbers for DAQ server (to prevent conflict)

--- matlab/runforcesensorsserver.m
+++ matlab/runforcesensorsserver.m
@@ -1,6 +1,6 @@
-% RUNFORCESENSORSSERVER - run a force sensors server on port 3015 in another matlab process
+% RUNFORCESENSORSSERVER - run a force sensors server on port 3016 in another matlab process
 % The sensors are specificied on channels 0 to 4, with range "4" (-1V to +1V, see cbw.h for definitions)
-function runforcesensorsserver(parameters,numsensors)
+function runforcesensorsserver(parameters,numsensors,firstsensor)
 
 if nargin<1 || isempty(parameters)
     parameters = '[0 0 0 0 0;1 1 1 1 1]'; % i.e. record the voltage
@@ -10,6 +10,10 @@ if nargin<2
     numsensors = 5;
 end
 
+if nargin<3
+    firstsensor = 0;
+end
+
 % for range, 4 = +- 1V, 0 = +- 5V
-system(['matlab -nojvm -r "d = forcesensorsserver(3015,6000,[0 ' num2str(numsensors-1) '],0,' parameters ',1);listen(d)" &']);
+system(['matlab -nojvm -r "d = forcesensorsserver(3016,6000,[' num2str(firstsensor) ' ' num2str(firstsensor+numsensors-1) '],0,' parameters ',1);listen(d)" &']);
 
