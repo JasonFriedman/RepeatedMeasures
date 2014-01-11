42799dc79a9c6f1fbecdb8139bfc7b56557c7f49
Jason Friedman <write.to.jason@gmail.com>
2014-01-11 19:19:00 +0200

    Prevent crashing when using keyboardresponse
    without keyboardclient

--- matlab/@keyboardresponse/feedback.m
+++ matlab/@keyboardresponse/feedback.m
@@ -6,6 +6,9 @@ function thistrial = feedback(r,e,thistrial,previoustrial,experimentdata,dataSum
 LEFT = KbName('z');
 RIGHT = KbName('m');
 
-firstpressed = dataSummary.kb_firstpressed;
-RT = dataSummary.kb_RT;
-thistrial = feedbackCommon(r,e,thistrial,RT,firstpressed,LEFT,RIGHT,experimentdata);
+% Feedback will only shown when using the keyboard server
+if isfield(dataSummary,'kb_firstpressed')
+    firstpressed = dataSummary.kb_firstpressed;
+    RT = dataSummary.kb_RT;
+    thistrial = feedbackCommon(r,e,thistrial,RT,firstpressed,LEFT,RIGHT,experimentdata);
+end
