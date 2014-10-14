3caef40854b5c0a7dd141fbc785a1ab6992a57c4
Jason Friedman <write.to.jason@gmail.com>
2014-10-14 16:38:03 +0300

    Documentation update

--- matlab/@libertyserver/private/compilecode.m
+++ matlab/@libertyserver/private/compilecode.m
@@ -1,7 +1,8 @@
 % compile code
 
 if strcmp(mexext,'mexw32')
-        mex -v LibertyMex.cpp -I"C:\Program Files (x86)\Polhemus\PDI\PDI_100\Inc" -L"C:\Program Files (x86)\Polhemus\PDI\PDI_100\lib\Win32\" -lPDI
+        %mex -v LibertyMex.cpp -I"C:\Program Files (x86)\Polhemus\PDI\PDI_100\Inc" -L"C:\Program Files (x86)\Polhemus\PDI\PDI_100\lib\Win32\" -lPDI
+        mex -v LibertyMex.cpp -I"C:\Program Files (x86)\Polhemus\PDI\PDI_90\Inc" -L"C:\Program Files (x86)\Polhemus\PDI\PDI_90\lib\Win32\" -lPDI
 elseif strcmp(mexext,'mexw64');
     mex -v LibertyMex.cpp -I"C:\Program Files (x86)\Polhemus\PDI\PDI_100\Inc" -L"C:\Program Files (x86)\Polhemus\PDI\PDI_100\lib\x64\" -lPDI
 else
