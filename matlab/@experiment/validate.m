cef09cc43e82d38d64f39bc1dd0aa852fde88d93
Jason Friedman <write.to.jason@gmail.com>
2014-12-24 11:19:47 +0200

    Add check for duplicate filenames
    Quit with an error in validation if duplicate filenames are found (to
    prevent files being accidentally overwritten)

--- matlab/@experiment/validate.m
+++ matlab/@experiment/validate.m
@@ -23,6 +23,25 @@ for k=1:length(devicelist)
     eval(['e.devices.' devicelist{k} ' = ' devicelist{k} 'client(clientparameters,e,-1);']);
 end
 
+count = 0;
 for k=1:numel(p.trial)
-    readTrialParameters(e,p.trial{k},experimentdata,1);
+    thistrial = readTrialParameters(e,p.trial{k},experimentdata,1);
+    if ~isempty(thistrial.filename)
+        count = count+1;
+        filenames{count} = thistrial.filename;
+    end
 end
+
+% Check for duplicate filenames (from
+% http://www.mathworks.com/matlabcentral/newsreader/view_thread/304505)
+[un idx_last idx] = unique(filenames);
+unique_idx = accumarray(idx(:),(1:length(idx))',[],@(x) {x});
+if any(cellfun(@length,unique_idx)>1)
+    for k=1:numel(unique_idx)
+        if numel(unique_idx{k})>0
+            fprintf('Filename %s is repeated %d times!\n',filenames{unique_idx{k}(1)},numel(unique_idx{k}));
+        end
+    end
+    error('Repeated filenames: Data will be overwritten!');
+end
+
