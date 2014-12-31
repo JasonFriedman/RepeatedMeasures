e7eedfbfecdeac4e911601009ed367f818a5a3de
Jason Friedman <write.to.jason@gmail.com>
2014-12-31 11:05:55 +0200

    Allow starting trial with external trigger
    currently only works with DAQ (Measurement computing DAQ card)

---
+++ matlab/@triggerstart/hasStarted.m
@@ -0,0 +1,29 @@
+% HASSTARTED - has the trial started?
+% Do not call directly, will be called by runexperiment
+
+function [started,keyCode] = hasStarted(m,e,experimentdata)
+
+devices = get(e,'devices');
+if isfield(devices,'DAQ')
+    lastsample = getsample(devices.DAQ);
+    if numel(lastsample)>1
+        lastsample = lastsample(2:end);
+    end
+else
+    MCtrigger = get(e,'MCtrigger');
+    lastsample = getdata(MCtrigger);
+    % convert to bits
+    lastsample = bitget(lastsample,1:8);
+end
+
+started = 0;
+if numel(lastsample)>0    
+    if m.up
+        started = lastsample(m.channel);
+    else
+        started = ~lastsample(m.channel);
+    end
+end
+
+% check the keyboard also
+[keyisdown,secs, keyCode ] = KbCheck;
---
+++ matlab/@triggerstart/triggerstart.m
@@ -0,0 +1,24 @@
+%TRIGGERSTART - class representing to start when an external trigger is received
+% e.g. from the DAQ card
+
+function [t,params] = triggerstart(inputParams,experimentdata)
+
+params.name = {'type','channel','up'};
+params.type = {'string','number','boolean'};
+params.description = {'Type of trigger to wait for. Currently the only option is ''DAQ''. There must either be a DAQ device defined, or setup.MCtrigger=1',...
+    'channel to wait for a trigger on (first channel = 1)',...
+    'whether the trigger is a high value (1) or a low value (0)'};
+params.required = [1 1 0];
+params.default = {'DAQ',1,1};
+params.classdescription = 'The trial starts when the an external trigger is received';
+params.classname = 'triggerstart';
+params.parentclassanme = 'start';
+
+if nargout>1
+    t = [];
+    return;
+end
+
+[t,parent] = readParameters(params,inputParams);
+
+t = class(t,'triggerstart',start(parent));
