b186983d286fd74febe52e87b842ca65d8b7a21f
Jason Friedman <write.to.jason@gmail.com>
2015-11-08 11:58:20 +0200

    Prevent crashing if trial is longer than colors in showPositionColor

--- matlab/@socketclient/showPositionCommon.m
+++ matlab/@socketclient/showPositionCommon.m
@@ -13,6 +13,9 @@ lastposition(2) = lastsample(2) * experimentdata.screenInfo.screenRect(4);
 if iscell(m.showPositionColor)
     thisShowPositionColor = m.showPositionColor{thistrial.trialnum};
     if size(thisShowPositionColor,1)>2
+        if frame > size(thisShowPositionColor,1)
+            frame = size(thisShowPositionColor,1);
+        end
         thisShowPositionColor = thisShowPositionColor(frame,:);
     end
     therow = 1;
