8eabd460f5ad60289183760aff9d052c68c5744f
Jason Friedman <write.to.jason@gmail.com>
2015-12-24 12:01:02 +0200

    Make trakStar work with >4 sensors

--- matlab/@trakStarserver/getsinglesample.m
+++ matlab/@trakStarserver/getsinglesample.m
@@ -8,11 +8,30 @@
 
 function [data,framenumber] = getsinglesample(ts,nummarkers)
 
-% Initialize a struct to put the results in 
-result = struct(libstruct('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Four'));
-resultPointer = libpointer('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Four', result);
-% Read all 4 markers (even if not connected)
-handleError(ts,calllib(ts.libstring, 'GetAsynchronousRecord',  hex2dec('ffff'), resultPointer, 64*4));
+% Initialize a struct to put the results in
+if nummarkers<=4
+    result = struct(libstruct('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Four'));
+    resultPointer = libpointer('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Four', result);
+    % Read all 4 markers (even if not connected)
+    handleError(ts,calllib(ts.libstring, 'GetSynchronousRecord',  hex2dec('ffff'), resultPointer, 64*4));
+elseif nummarkers<=8
+    result = struct(libstruct('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Eight'));
+    resultPointer = libpointer('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Eight', result);
+    % Read all 8 markers (even if not connected)
+    handleError(ts,calllib(ts.libstring, 'GetSynchronousRecord',  hex2dec('ffff'), resultPointer, 64*8));
+elseif nummarkers<=12
+    result = struct(libstruct('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Twelve'));
+    resultPointer = libpointer('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Twelve', result);
+    % Read all 12 markers (even if not connected)
+    handleError(ts,calllib(ts.libstring, 'GetSynchronousRecord',  hex2dec('ffff'), resultPointer, 64*12));
+elseif nummarkers<=16
+    result = struct(libstruct('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Sixteen'));
+    resultPointer = libpointer('tagDOUBLE_POSITION_ANGLES_TIME_Q_RECORD_AllSensors_Sixteen', result);
+    % Read all 16 markers (even if not connected)
+    handleError(ts,calllib(ts.libstring, 'GetSynchronousRecord',  hex2dec('ffff'), resultPointer, 64*16));
+else
+    error('Can not be more than 16 markers');
+end
 
 result = get(resultPointer,'value');
 
