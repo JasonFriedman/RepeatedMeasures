da312d7c7cddfba8f170ca0041bc02aeb9524c22
Jason Friedman <write.to.jason@gmail.com>
2016-01-25 09:53:11 +0530

    Use audioplayer in place of wavplay
    doesn't work in newer versions of Matlab

--- matlab/@experiment/private/playAnnoyingBeep.m
+++ matlab/@experiment/private/playAnnoyingBeep.m
@@ -1,8 +1,8 @@
 % PLAYANNOYINGBEEP - play an annoying beep (e.g. if did not move in time)
 function playAnnoyingBeep(experimentdata)
 
-if ispc
-    wavplay(experimentdata.annoyingBeep,experimentdata.annoyingBeepf);
-else
+%if ispc
+%    wavplay(experimentdata.annoyingBeep,experimentdata.annoyingBeepf);
+%else
     audioplayer(experimentdata.annoyingBeep,experimentdata.annoyingBeepf);
-end
+%end
