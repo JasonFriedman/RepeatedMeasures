136e09ec8daed7a21684c56e872346d26a404cd9
Jason Friedman <write.to.jason@gmail.com>
2016-07-03 16:15:07 +0300

    Updated get functions to work with superclass

--- matlab/@libertyclient/get.m
+++ matlab/@libertyclient/get.m
@@ -1,20 +1,35 @@
-% GET - generic get function (for array)
+% GET - generic get function (for classes, will look in superclass)
 %
 % result = get(h,field)
 
 function result = get(h,field)
 
 if nargin==2
-    if max(size(h))==1
-        eval(['result = h.' field ';']);
-    else
-        for i=1:size(h,1)
-            for j=1:size(h,2)
-                eval(['result(i,j) = h(i,j).' field ';']);
-            end
+    thefields = fields(h);
+    isafield = 0;
+    superclass = [];
+    for k=1:numel(thefields)
+        if strcmp(field,thefields{k})
+            isafield = 1;
+        elseif isa(h.(thefields{k}),thefields{k}) % if this is a class
+            superclass = h.(thefields{k});
         end
     end
-        
+    if isafield
+        if max(size(h))==1
+            eval(['result = h.' field ';']);
+        else
+            for i=1:size(h,1)
+                for j=1:size(h,2)
+                    eval(['result(i,j) = h(i,j).' field ';']);
+                end
+            end
+        end
+    elseif ~isempty(superclass)
+        result = get(superclass,field);
+    else
+        error(['There is no field ' field ' in this object']);
+    end
 elseif nargin==1
     result = fields(h);
 end
