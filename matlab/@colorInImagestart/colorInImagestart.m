d8f1c7d2518d7259969dde864a0c2712d6ddb693
Jason Friedman <write.to.jason@gmail.com>
2017-01-25 12:10:19 +0200

    Documentation update

--- matlab/@colorInImagestart/colorInImagestart.m
+++ matlab/@colorInImagestart/colorInImagestart.m
@@ -14,7 +14,7 @@ params.type = {'number','matrix_1_3','number','number'};
 params.description = {'Image number to use (from those defined in setup)',...
     'color in the image that signfies the target',...
     'The amount of time you need to be at the target for (in seconds)','allowable cityblock distance from the color'};
-params.required = [1 1 0 9];
+params.required = [1 1 0 0];
 params.default = {1,[0 0 0],0,0};
 params.classdescription = 'The start position are defined by a color in an image';
 params.classname = 'colorInImagestart';
