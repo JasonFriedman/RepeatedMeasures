868cf722f0469c03fadea32c91b719ad171dde37
Jason Friedman <write.to.jason@gmail.com>
2022-08-10 12:32:01 +0300

    attempt to prevent crash when opening video file
    Try to read video file twice (sometimes helps avoid crash)

--- matlab/@videostimulus/setup.m
+++ matlab/@videostimulus/setup.m
@@ -7,12 +7,22 @@ videoFilename = [pwd '/stimuli/' s.filename];
 % The -1 signifies to put the whole movie into RAM, without
 % this it did not seem to be always loading the whole
 % video.
-[ thistrial.moviePtr, duration, thistrial.video_fps, width, height, thistrial.video_frames] = ...
-    Screen('OpenMovie', experimentdata.screenInfo.curWindow, videoFilename, 0 , -1);
+try
+   Screen('CloseMovie'); % close any open movies 
+   [ thistrial.moviePtr, duration, thistrial.video_fps, width, height, thistrial.video_frames] = ...
+       Screen('OpenMovie', experimentdata.screenInfo.curWindow, videoFilename, 0 , -1);
+catch
+    fprintf('Caught error trying to open movie, trying again\n');
+    pause(1);
+    [ thistrial.moviePtr, duration, thistrial.video_fps, width, height, thistrial.video_frames] = ...
+       Screen('OpenMovie', experimentdata.screenInfo.curWindow, videoFilename, 0 , -1);
+end
+
 if thistrial.moviePtr==-1
     error(['Could not load file ' videoFilename]);
 end
 Screen('SetMovieTimeIndex', thistrial.moviePtr, 0);
 thistrial.stimuliFrames = thistrial.video_frames+1; % extra frame to close the video and release the memory
 fprintf('There are %d frames in the video\n',thistrial.stimuliFrames-1);
+
 
